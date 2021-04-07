4b6c265283f6cf94e43d6b4b348d3e152e1caef8
tersc <tersc@belfius.be>
2021-04-07 08:02:04 +0200

    extra comment

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -14,8 +14,9 @@ class BeerDtoTest extends BaseTest{
         BeerDto beerDto = getDto();
 
         String jsonString = objectMapper.writeValueAsString(beerDto);
-
+        System.out.println("------serialize----------");
         System.out.println(jsonString);
+        System.out.println("----------------");
     }
 
     @Test
@@ -23,8 +24,8 @@ class BeerDtoTest extends BaseTest{
         String json = "{\"beerId\":\"639c00ff-9993-4ec1-af12-8277c85f93bb\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2019-06-02T16:35:58.321001-04:00\",\"lastUpdatedDate\":\"2019-06-02T16:35:58.321872-04:00\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
-
+        System.out.println("------de-ser---------");
         System.out.println(dto);
-
+        System.out.println("----------------");
     }
 }
